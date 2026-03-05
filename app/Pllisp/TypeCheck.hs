469b2d5735bff4a48849022c2fce3c8f192dd671
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-03-05 09:54:35 -0500

    minor refactor

--- app/Pllisp/TypeCheck.hs
+++ app/Pllisp/TypeCheck.hs
@@ -12,55 +12,32 @@ import qualified Control.Monad.RWS as RWS
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
--- ENTRY POINT
+-- TYPES
 
-typecheck :: Res.ResolvedCST -> Either TypeError TResolvedCST
-typecheck exprs = do
-  let typeDecls = extractTypeDecls exprs
-      ctorCtx = buildCtorContext typeDecls
-      builtInCtx = M.map (uncurry Forall) BuiltIn.builtInSchemes
-      initialCtx = M.union ctorCtx builtInCtx
-  (typed, _, constraints) <- RWS.runRWST (traverse infer exprs) initialCtx 0
-  subst <- solve constraints
-  Right (apply subst typed)
+type TyVar = Integer
 
-extractTypeDecls :: [Res.RExpr] -> [(CST.Symbol, [CST.Symbol], [CST.DataCon])]
-extractTypeDecls = foldr go []
-  where
-    go (Loc.Located _ (Res.RType name params ctors)) acc = (name, params, ctors) : acc
-    go _ acc = acc
+type Subst = M.Map TyVar Ty.Type
 
-buildCtorContext :: [(CST.Symbol, [CST.Symbol], [CST.DataCon])] -> Context
-buildCtorContext = M.fromList . concatMap buildCtors
-  where
-    buildCtors (typeName, params, ctors) =
-      let paramMap = M.fromList (zip params [0..])
-          paramTyVars = map Ty.TyVar [0 .. fromIntegral (length params - 1)]
-          resultTy = Ty.TyCon typeName paramTyVars
-          paramSet = S.fromList [0 .. fromIntegral (length params - 1)]
-      in map (buildCtor paramMap paramSet resultTy) ctors
+type Constraints = [Constraint]
 
-    buildCtor paramMap paramSet resultTy (CST.DataCon ctorName args) =
-      let argTys = map (resolveTypeParams paramMap) args
-          ctorTy = if null argTys
-                   then resultTy
-                   else Ty.TyFun argTys resultTy
-          scheme = Forall paramSet ctorTy
-      in (ctorName, scheme)
+type Context = M.Map CST.Symbol Scheme
 
-    resolveTypeParams paramMap ty = case ty of
-      Ty.TyCon name [] -> case M.lookup name paramMap of
-        Just idx -> Ty.TyVar idx
-        Nothing  -> ty
-      Ty.TyCon name args -> Ty.TyCon name (map (resolveTypeParams paramMap) args)
-      Ty.TyFun args ret -> Ty.TyFun (map (resolveTypeParams paramMap) args) (resolveTypeParams paramMap ret)
-      _ -> ty
+type Solve a = Either TypeError a
 
--- CORE
+type Infer a = RWS.RWST Context Constraints TyVar (Either TypeError) a
 
 type TResolvedCST = [TRExpr]
 
 type TRExpr = Loc.Located (Ty.Typed TRExprF)
+
+data Scheme = Forall (S.Set TyVar) Ty.Type
+
+data Constraint = Constraint Ty.Type Ty.Type
+
+data TypeError = TypeError
+  { teSpan :: Loc.Span
+  , teMsg  :: String
+  }
 
 data TRExprF
   = TRLit  CST.Literal
@@ -73,139 +50,7 @@ data TRExprF
   | TRType CST.Symbol [CST.Symbol] [CST.DataCon]
   deriving (Eq, Show)
 
-data TypeError = TypeError
-  { teSpan :: Loc.Span
-  , teMsg  :: String
-  }
-
-infer :: Res.RExpr -> Infer TRExpr
-infer (Loc.Located sp expr) = Loc.Located sp <$> case expr of
-  Res.RLit l@(CST.LitInt _) -> do
-    pure $ Ty.Typed Ty.TyInt (TRLit l)
-  Res.RLit l@(CST.LitFlt _) -> do
-    pure $ Ty.Typed Ty.TyFlt (TRLit l)
-  Res.RLit l@(CST.LitStr _) -> do
-    pure $ Ty.Typed Ty.TyStr (TRLit l)
-  Res.RBool b -> do
-    pure $ Ty.Typed Ty.TyBool (TRBool b)
-  Res.RVar vb -> do
-    ctx <- RWS.ask
-    case M.lookup (Res.symName vb) ctx of
-      Just scheme -> do
-        t <- instantiate scheme
-        pure $ Ty.Typed t (TRVar vb)
-      Nothing -> throwTE sp ("Undefined variable " ++ show (Res.symName vb))
-  Res.RIf c t e -> do
-    ct <- infer c
-    tt <- infer t
-    et <- infer e
-    constrain (typeOf ct) Ty.TyBool
-    constrain (typeOf tt) (typeOf et)
-    pure $ Ty.Typed (typeOf tt) (TRIf ct tt et)
-  Res.RApp fexpr aexprs -> do
-    ft <- infer fexpr
-    ats <- traverse infer aexprs
-    rt <- fresh
-    constrain (typeOf ft) (Ty.TyFun (map typeOf ats) rt)
-    pure $ Ty.Typed rt (TRApp ft ats)
-  Res.RLam params _mRet body -> do
-    paramTys <- traverse paramType params
-    let paramNames = map CST.symName params
-        paramSchemes = map (Forall S.empty) paramTys
-        newBindings = M.fromList (zip paramNames paramSchemes)
-    bodyExpr <- RWS.local (M.union newBindings) (infer body)
-    let funTy = Ty.TyFun paramTys (typeOf bodyExpr)
-    pure $ Ty.Typed funTy (TRLam (zip paramNames paramTys) (typeOf bodyExpr) bodyExpr)
-  Res.RLet binds body -> do
-    ctx <- RWS.ask
-    let names = map (CST.symName . fst) binds
-    freshTys <- traverse (const fresh) names
-    let monoSchemes = map (Forall S.empty) freshTys
-        recCtx = M.union (M.fromList (zip names monoSchemes)) ctx
-    rhsExprs <- traverse (\(_, rhs) -> RWS.local (const recCtx) (infer rhs)) binds
-    sequence_ $ zipWith constrain freshTys (map typeOf rhsExprs)
-    let generalizedSchemes = map (generalize ctx . typeOf) rhsExprs
-        bodyCtx = M.union (M.fromList (zip names generalizedSchemes)) ctx
-    bodyExpr <- RWS.local (const bodyCtx) (infer body)
-    let typedBinds = zip3 names (map typeOf rhsExprs) rhsExprs
-    pure $ Ty.Typed (typeOf bodyExpr) (TRLet typedBinds bodyExpr)
-  Res.RType name params ctors -> do
-    -- Constructors are registered in context by typecheck, just pass through here
-    pure $ Ty.Typed (Ty.TyCon name []) (TRType name params ctors)
-
-unify :: Ty.Type -> Ty.Type -> Solve Subst
-unify (Ty.TyVar v) t = bind v t
-unify t (Ty.TyVar v) = bind v t
-unify (Ty.TyFun as1 r1) (Ty.TyFun as2 r2) = unifyMany (r1:as1) (r2:as2)
-unify (Ty.TyCon n1 ts1) (Ty.TyCon n2 ts2)
-  | n1 == n2  = unifyMany ts1 ts2
-unify Ty.TyInt Ty.TyInt = Right M.empty
-unify Ty.TyFlt Ty.TyFlt = Right M.empty
-unify Ty.TyStr Ty.TyStr = Right M.empty
-unify Ty.TyBool Ty.TyBool = Right M.empty
-unify t1 t2 = Left $ solveErr ("cannot unify " ++ show t1 ++ " with " ++ show t2)
-
-unifyMany :: [Ty.Type] -> [Ty.Type] -> Solve Subst
-unifyMany [] [] = Right M.empty
-unifyMany (t1:ts1) (t2:ts2) = do
-  s1 <- unify t1 t2
-  s2 <- unifyMany (apply s1 ts1) (apply s1 ts2)
-  Right (compose s2 s1)
-unifyMany _ _ = Left $ solveErr "type mismatch: different arities"
-
-bind :: TyVar -> Ty.Type -> Solve Subst
-bind tv t
-  | tv `S.member` tvs t = Left $ solveErr ("infinite type " ++ show tv ++ " ~ " ++ show t)
-  | otherwise = Right $ M.singleton tv t
-
-solveErr :: String -> TypeError
-solveErr msg = TypeError (Loc.Span p p) msg
-  where p = Loc.Pos "" 0 0
-
-solve :: Constraints -> Solve Subst
-solve [] = Right M.empty
-solve (Constraint t1 t2 : cs) = do
-  s1 <- unify t1 t2
-  s2 <- solve (apply s1 cs)
-  Right (compose s2 s1)
-
--- HELPERS
-
-type Solve a = Either TypeError a
-
-data Scheme = Forall (S.Set TyVar) Ty.Type
-
-data Constraint = Constraint Ty.Type Ty.Type
-
-type Context = M.Map CST.Symbol Scheme
-
-type Constraints = [Constraint]
-
-type TyVar = Integer
-
-type Subst = M.Map TyVar Ty.Type
-
-type Infer a = RWS.RWST Context Constraints TyVar (Either TypeError) a
-
-throwTE :: Loc.Span -> String -> Infer a
-throwTE sp msg = RWS.lift . Left $ TypeError sp msg
-
-paramType :: CST.TSymbol -> Infer Ty.Type
-paramType tsym = case CST.symType tsym of
-  Just t  -> pure t
-  Nothing -> fresh
-
-typeOf :: TRExpr -> Ty.Type
-typeOf (Loc.Located _ (Ty.Typed t _)) = t
-
-fresh :: Infer Ty.Type
-fresh = RWS.state (\n -> (Ty.TyVar n, n+1))
-
-constrain :: Ty.Type -> Ty.Type -> Infer ()
-constrain t1 t2 = RWS.tell [Constraint t1 t2]
-
-compose :: Subst -> Subst -> Subst
-compose a b = M.map (apply a) (b `M.union` a)
+-- SUBSTITUTION
 
 class Substitutable a where
   apply :: Subst -> a -> a
@@ -261,6 +106,131 @@ instance Substitutable TRExprF where
   apply s (TRApp f as) = TRApp (apply s f) (apply s as)
   apply _ (TRType n ps cs) = TRType n ps cs
 
+compose :: Subst -> Subst -> Subst
+compose a b = M.map (apply a) (b `M.union` a)
+
+-- ENTRY POINT
+
+typecheck :: Res.ResolvedCST -> Either TypeError TResolvedCST
+typecheck exprs = do
+  let typeDecls = extractTypeDecls exprs
+      ctorCtx = buildCtorContext typeDecls
+      builtInCtx = M.map (uncurry Forall) BuiltIn.builtInSchemes
+      initialCtx = M.union ctorCtx builtInCtx
+  (typed, _, constraints) <- RWS.runRWST (traverse infer exprs) initialCtx 0
+  subst <- solve constraints
+  Right (apply subst typed)
+
+-- CONTEXT BUILDING
+
+extractTypeDecls :: [Res.RExpr] -> [(CST.Symbol, [CST.Symbol], [CST.DataCon])]
+extractTypeDecls = foldr go []
+  where
+    go (Loc.Located _ (Res.RType name params ctors)) acc = (name, params, ctors) : acc
+    go _ acc = acc
+
+buildCtorContext :: [(CST.Symbol, [CST.Symbol], [CST.DataCon])] -> Context
+buildCtorContext = M.fromList . concatMap buildCtors
+  where
+    buildCtors (typeName, params, ctors) =
+      let paramMap = M.fromList (zip params [0..])
+          paramTyVars = map Ty.TyVar [0 .. fromIntegral (length params - 1)]
+          resultTy = Ty.TyCon typeName paramTyVars
+          paramSet = S.fromList [0 .. fromIntegral (length params - 1)]
+      in map (buildCtor paramMap paramSet resultTy) ctors
+
+    buildCtor paramMap paramSet resultTy (CST.DataCon ctorName args) =
+      let argTys = map (resolveTypeParams paramMap) args
+          ctorTy = if null argTys
+                   then resultTy
+                   else Ty.TyFun argTys resultTy
+          scheme = Forall paramSet ctorTy
+      in (ctorName, scheme)
+
+    resolveTypeParams paramMap ty = case ty of
+      Ty.TyCon name [] -> case M.lookup name paramMap of
+        Just idx -> Ty.TyVar idx
+        Nothing  -> ty
+      Ty.TyCon name args -> Ty.TyCon name (map (resolveTypeParams paramMap) args)
+      Ty.TyFun args ret -> Ty.TyFun (map (resolveTypeParams paramMap) args) (resolveTypeParams paramMap ret)
+      _ -> ty
+
+-- INFERENCE
+
+infer :: Res.RExpr -> Infer TRExpr
+infer (Loc.Located sp expr) = Loc.Located sp <$> case expr of
+  Res.RLit l@(CST.LitInt _) ->
+    pure $ Ty.Typed Ty.TyInt (TRLit l)
+  Res.RLit l@(CST.LitFlt _) ->
+    pure $ Ty.Typed Ty.TyFlt (TRLit l)
+  Res.RLit l@(CST.LitStr _) ->
+    pure $ Ty.Typed Ty.TyStr (TRLit l)
+  Res.RBool b ->
+    pure $ Ty.Typed Ty.TyBool (TRBool b)
+  Res.RVar vb -> do
+    ctx <- RWS.ask
+    case M.lookup (Res.symName vb) ctx of
+      Just scheme -> do
+        t <- instantiate scheme
+        pure $ Ty.Typed t (TRVar vb)
+      Nothing -> throwTE sp ("Undefined variable " ++ show (Res.symName vb))
+  Res.RIf c t e -> do
+    ct <- infer c
+    tt <- infer t
+    et <- infer e
+    constrain (typeOf ct) Ty.TyBool
+    constrain (typeOf tt) (typeOf et)
+    pure $ Ty.Typed (typeOf tt) (TRIf ct tt et)
+  Res.RApp fexpr aexprs -> do
+    ft <- infer fexpr
+    ats <- traverse infer aexprs
+    rt <- fresh
+    constrain (typeOf ft) (Ty.TyFun (map typeOf ats) rt)
+    pure $ Ty.Typed rt (TRApp ft ats)
+  Res.RLam params _mRet body -> do
+    paramTys <- traverse paramType params
+    let paramNames = map CST.symName params
+        paramSchemes = map (Forall S.empty) paramTys
+        newBindings = M.fromList (zip paramNames paramSchemes)
+    bodyExpr <- RWS.local (M.union newBindings) (infer body)
+    let funTy = Ty.TyFun paramTys (typeOf bodyExpr)
+    pure $ Ty.Typed funTy (TRLam (zip paramNames paramTys) (typeOf bodyExpr) bodyExpr)
+  Res.RLet binds body -> do
+    ctx <- RWS.ask
+    let names = map (CST.symName . fst) binds
+    freshTys <- traverse (const fresh) names
+    let monoSchemes = map (Forall S.empty) freshTys
+        recCtx = M.union (M.fromList (zip names monoSchemes)) ctx
+    rhsExprs <- traverse (\(_, rhs) -> RWS.local (const recCtx) (infer rhs)) binds
+    sequence_ $ zipWith constrain freshTys (map typeOf rhsExprs)
+    let generalizedSchemes = map (generalize ctx . typeOf) rhsExprs
+        bodyCtx = M.union (M.fromList (zip names generalizedSchemes)) ctx
+    bodyExpr <- RWS.local (const bodyCtx) (infer body)
+    let typedBinds = zip3 names (map typeOf rhsExprs) rhsExprs
+    pure $ Ty.Typed (typeOf bodyExpr) (TRLet typedBinds bodyExpr)
+  Res.RType name params ctors ->
+    -- Constructors are registered in context by typecheck, just pass through here
+    pure $ Ty.Typed (Ty.TyCon name []) (TRType name params ctors)
+
+-- Inference helpers
+
+fresh :: Infer Ty.Type
+fresh = RWS.state (\n -> (Ty.TyVar n, n+1))
+
+constrain :: Ty.Type -> Ty.Type -> Infer ()
+constrain t1 t2 = RWS.tell [Constraint t1 t2]
+
+throwTE :: Loc.Span -> String -> Infer a
+throwTE sp msg = RWS.lift . Left $ TypeError sp msg
+
+paramType :: CST.TSymbol -> Infer Ty.Type
+paramType tsym = case CST.symType tsym of
+  Just t  -> pure t
+  Nothing -> fresh
+
+typeOf :: TRExpr -> Ty.Type
+typeOf (Loc.Located _ (Ty.Typed t _)) = t
+
 generalize :: Context -> Ty.Type -> Scheme
 generalize ctx t = Forall (tvs t `S.difference` tvs (M.elems ctx)) t
 
@@ -270,3 +240,41 @@ instantiate (Forall vs t) = do
   ftvs <- traverse (const fresh) vars
   let subst = M.fromList (zip vars ftvs)
   return $ apply subst t
+
+-- UNIFICATION / SOLVING
+
+unify :: Ty.Type -> Ty.Type -> Solve Subst
+unify (Ty.TyVar v) t = bind v t
+unify t (Ty.TyVar v) = bind v t
+unify (Ty.TyFun as1 r1) (Ty.TyFun as2 r2) = unifyMany (r1:as1) (r2:as2)
+unify (Ty.TyCon n1 ts1) (Ty.TyCon n2 ts2)
+  | n1 == n2  = unifyMany ts1 ts2
+unify Ty.TyInt Ty.TyInt = Right M.empty
+unify Ty.TyFlt Ty.TyFlt = Right M.empty
+unify Ty.TyStr Ty.TyStr = Right M.empty
+unify Ty.TyBool Ty.TyBool = Right M.empty
+unify t1 t2 = Left $ solveErr ("cannot unify " ++ show t1 ++ " with " ++ show t2)
+
+unifyMany :: [Ty.Type] -> [Ty.Type] -> Solve Subst
+unifyMany [] [] = Right M.empty
+unifyMany (t1:ts1) (t2:ts2) = do
+  s1 <- unify t1 t2
+  s2 <- unifyMany (apply s1 ts1) (apply s1 ts2)
+  Right (compose s2 s1)
+unifyMany _ _ = Left $ solveErr "type mismatch: different arities"
+
+bind :: TyVar -> Ty.Type -> Solve Subst
+bind tv t
+  | tv `S.member` tvs t = Left $ solveErr ("infinite type " ++ show tv ++ " ~ " ++ show t)
+  | otherwise = Right $ M.singleton tv t
+
+solve :: Constraints -> Solve Subst
+solve [] = Right M.empty
+solve (Constraint t1 t2 : cs) = do
+  s1 <- unify t1 t2
+  s2 <- solve (apply s1 cs)
+  Right (compose s2 s1)
+
+solveErr :: String -> TypeError
+solveErr msg = TypeError (Loc.Span p p) msg
+  where p = Loc.Pos "" 0 0
